73c706fbccaedd0d5e38f4e02743d88b46738aff
Alberto Carreras <acarrerasc@gmail.com>
2018-04-25 03:24:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
-class Dog(name_dog, breed_dog = "Mutt")
-  def initialize(name_dog, breed_dog)
+class Dog
+  def initialize(name_dog, breed_dog = "Mutt")
     @name = name_dog
     @breed = breed_dog
   end
