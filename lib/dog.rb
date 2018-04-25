a111d6409ea71386a13c8c48b3149a193577c39d
Alberto Carreras <acarrerasc@gmail.com>
2018-04-25 03:23:15 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog(name_dog, breed_dog = "Mutt")
+  def initialize(name_dog, breed_dog)
+    @name = name_dog
+    @breed = breed_dog
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person(name_person)
+  def initialize(name_person)
+    @name = name_person
+  end
+end
